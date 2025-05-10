42ac6cfd186a21a04cf17d9816208b43a6ba9cc0
JenJen123123 <ssystem32@gmail.com>
2025-05-10 19:30:37 +0300

    vars

--- createBuckets.py
+++ createBuckets.py
@@ -1,3 +1,10 @@
 import boto3
 
 print("\n************ Creating Two Buckets ************\n")
+
+bucket_name1 = 'dct-sales'
+bucket_name2 = 'dct-sales-sr1'
+region = 'us-west-2'
+
+client = boto3.client("s3")
+
